998dddd9e65b14cbdae5f805d52fcd1235b6a35f
Yasantha Kalupahana <yasa.kalupahana@gmail.com>
2019-11-16 10:26:16 +1100

    git commit -a -m 'setting up initial project'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CryptoCurrencyDashboardComponent } from './crypto-currency-dashboard/crypto-currency-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CryptoCurrencyDashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/crypto-currency-dashboard/crypto-currency-dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-crypto-currency-dashboard',
+    templateUrl: './crypto-currency-dashboard.component.html',
+    styleUrls: ['./crypto-currency-dashboard.component.scss']
+  })
+
+  export class CryptoCurrencyDashboardComponent implements OnInit {
+
+
+    ngOnInit() {
+  
+    
+    }
+
+  }
