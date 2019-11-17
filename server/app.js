8f39a9a6b7f700d9aa9ebed796e5de5c86bae697
Yasantha Kalupahana <yasa.kalupahana@gmail.com>
2019-11-18 09:56:07 +1100

    git commit -m 'adding a sepearate file to retrieve json data'

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+var fs = require('fs');
+var datafile = 'server/data/currency.json';
 const app = express();
 
 app.use((req, res, next) => {
@@ -16,87 +17,9 @@ app.use((req, res, next) => {
 });
 
 app.get("/api/currencies", (req, res, next) => {
-  const currencies =  [
-    {
-        "currency": "BTC",
-        "date": "20180507",
-        "quotes": [
-            {
-                "time": "0915",
-                "price": "34.98"
-            },
-            {
-                "time": "1045",
-                "price": "36.13"
-            },
-            {
-                "time": "1230",
-                "price": "37.01"
-            },
-            {
-                "time": "1400",
-                "price": "35.98"
-            },
-            {
-                "time": "1530",
-                "price": "33.56"
-            }
-        ]
-    },
-    {
-        "currency": "ETC",
-        "date": "20180507",
-        "quotes": [
-            {
-                "time": "0900",
-                "price": "1.45"
-            },
-            {
-                "time": "1030",
-                "price": "1.87"
-            },
-            {
-                "time": "1245",
-                "price": "1.55"
-            },
-            {
-                "time": "1515",
-                "price": "2.01"
-            },
-            {
-                "time": "1700",
-                "price": "2.15"
-            }
-        ]
-    },
-    {
-        "currency": "LTC",
-        "date": "20180507",
-        "quotes": [
-            {
-                "time": "0930",
-                "price": "14.32"
-            },
-            {
-                "time": "1115",
-                "price": "14.87"
-            },
-            {
-                "time": "1245",
-                "price": "15.03"
-            },
-            {
-                "time": "1400",
-                "price": "14.76"
-            },
-            {
-                "time": "1700",
-                "price": "14.15"
-            }
-        ]
-    }
-]
-  res.status(200).json(currencies);
+
+const data = fs.readFileSync(datafile, 'utf8');
+  res.status(200).json(JSON.parse(data));
 });
 
 module.exports = app;
